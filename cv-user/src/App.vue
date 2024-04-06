1a9cff854e0541a326feebb8d7de4db7a0fcf67f
rodrigo1990 <mcd77.1990@gmail.com>
2024-04-07 00:47:10 +0200

    apply general styles

--- cv-user/src/App.vue
+++ cv-user/src/App.vue
@@ -1,27 +1,36 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Curriculum App"/>
+  <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  </head>
+  <html lang="en">
+    <HeaderMain/>
+    <ButtonsColumn :buttonsArray="['Whoami','Academics','Services','Github']"/>
+  </html>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+
+import ButtonsColumn from './components/ButtonsColumn.vue'
+import HeaderMain from './components/HeaderMain.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    ButtonsColumn,
+    HeaderMain
   }
 }
 </script>
 
 <style>
 body {
+  margin:0;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: white;
-  margin-top: 60px;
   background: rgb(74,34,0);
   background: linear-gradient(164deg, rgba(74,34,0,1) 0%, rgba(10,10,12,1) 50%);
   background-repeat: no-repeat;
