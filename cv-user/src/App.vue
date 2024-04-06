97cf2ae699ba4682676834e6d0567683fab9112b
rodrigo1990 <mcd77.1990@gmail.com>
2024-04-06 17:18:48 +0200

    set background color

--- cv-user/src/App.vue
+++ cv-user/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Heyyyyy to Your Vue.js App edited by rodri"/>
+  <HelloWorld msg="Heyy to Your Vue.js App edited by rodri"/>
 </template>
 
 <script>
@@ -15,12 +15,16 @@ export default {
 </script>
 
 <style>
-#app {
+body {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: white;
   margin-top: 60px;
+  background: rgb(74,34,0);
+  background: linear-gradient(164deg, rgba(74,34,0,1) 0%, rgba(10,10,12,1) 50%);
+  background-repeat: no-repeat;
+  background-attachment: fixed;
 }
 </style>
