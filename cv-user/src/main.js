58f2a47d8d7efbbe1037a14dfc0bac43bee87ab8
rodrigo1990 <mcd77.1990@gmail.com>
2024-04-07 14:22:37 +0200

    install vue-grid-responsive

--- cv-user/src/main.js
+++ cv-user/src/main.js
@@ -1,4 +1,8 @@
 import { createApp } from 'vue'
+import { Row, Column, Hidden } from 'vue-grid-responsive';
 import App from './App.vue'
 
 createApp(App).mount('#app')
+App.component('row', Row);
+App.component('column', Column);
+App.component('hidden', Hidden);
