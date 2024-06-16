a839ae5c1767c9fe1faa3dd3a3f3c930fcd54cb8
rodrigo1990 <mcd77.1990@gmail.com>
2024-06-16 05:40:08 +0200

    refactor to methods

--- cv-user/src/components/Content/Content.vue
+++ cv-user/src/components/Content/Content.vue
@@ -17,8 +17,25 @@ export default {
             data: null
         }
     },
+    async beforeRouteUpdate(to) {
+        this.setState(to)
+    },
     mounted(){
-        this.data = ['<p> I am a developer from Argentina, I like PHP etc. </p>']
+        this.setState(this.$route)
+    },
+    methods:{
+        setState(to){
+            if(to.params.id == 1){
+                this.data = ['<p> I am a developer from Argentina, I like PHP etc. </p>']
+            }else if(to.params.id == 2){
+                this.data = ['<p> Section 2 </p>']
+            }else if(to.params.id == 3){
+                this.data = ['<p> Section 3 </p>']
+            }
+            else{
+                this.data = ['<p> Something else </p>']
+            }
+        }
     }
 }
 </script>
