6970e4adccde2c61d59ec4793cff7d9a3495a5d9
rodrigo1990 <mcd77.1990@gmail.com>
2024-10-23 01:11:16 +0200

    refactor: move BodyLayout to app component

--- curriculum-frontend/app.vue
+++ curriculum-frontend/app.vue
@@ -1,7 +1,34 @@
 <template>
-  <div>
-    <NuxtLayout>
-      <NuxtPage />
-    </NuxtLayout>
+  <div class="row">
+    <div class="container">
+        
+      <div class="col-xs-12 col-md-1 col-lg-1"> 
+          <ButtonsColumn :buttonsArray="this.btnsData"/>
+      </div>
+        
+        <div class="col-xs-12 col-lg-4">
+         <!--   <ProfileImage path="pic.jpg"/> -->
+        </div>
+
+        <div class="col-xs-12 col-lg-7">
+          <NuxtLayout>
+            <NuxtPage />
+          </NuxtLayout>
+        </div>
+      </div>
   </div>
 </template>
+<script>
+import { BtnsBodyData } from '~/src/dummyData/BtnsBodyData';
+export default {
+    name: 'App',
+    data(){
+      return {
+          btnsData:BtnsBodyData
+      }
+    },
+}
+</script>
+<style scoped lang="scss">
+    @import url("~/assets/styles/body-layout.scss");
+</style>
