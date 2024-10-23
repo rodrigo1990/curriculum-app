0f7e4fe3bd0a68bdf0bde9108303bf99ff6b3776
rodrigo1990 <mcd77.1990@gmail.com>
2024-10-24 01:27:39 +0200

    Revert "refactor: move BodyLayout to app component"
    This reverts commit 6970e4adccde2c61d59ec4793cff7d9a3495a5d9.

--- curriculum-frontend/app.vue
+++ curriculum-frontend/app.vue
@@ -1,34 +1,7 @@
 <template>
-  <div class="container">
-    <div class="row">
-        
-      <div class="col-xs-12 col-lg-4"> 
-          <ButtonsColumn :buttonsArray="this.btnsData"/>
-      </div>
-        
-        <div class="col-xs-12 col-md-4">
-              <ProfileImage path="pic.jpg"/>
-        </div>
-
-        <div class="col-xs-12 col-md-4">
-          <NuxtLayout>
-            <NuxtPage />
-          </NuxtLayout>
-        </div>
-      </div>
+  <div>
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
   </div>
 </template>
-<script>
-import { BtnsBodyData } from '~/src/dummyData/BtnsBodyData';
-export default {
-    name: 'App',
-    data(){
-      return {
-          btnsData:BtnsBodyData
-      }
-    },
-}
-</script>
-<style scoped lang="scss">
-    @import url("~/assets/styles/body-layout.scss");
-</style>
---
+++ curriculum-frontend/components/BodyLayout.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="row">
+    <div class="container">
+        
+      <div class="col-xs-12 col-md-1 col-lg-1"> 
+          <ButtonsColumn :buttonsArray="this.btnsData"/>
+      </div>
+        
+        <div class="col-xs-12 col-lg-4">
+         <!--   <ProfileImage path="pic.jpg"/> -->
+        </div>
+
+        <div class="col-xs-12 col-lg-7">
+          <RouterView :key="$route.path"/>
+        </div>
+      </div>
+  </div>
+</template>
+
+<script>
+import { RouterView } from 'vue-router';
+import { BtnsBodyData } from '~/src/dummyData/BtnsBodyData';
+export default {
+    name: 'BodyMain',
+    data(){
+      return {
+          btnsData:BtnsBodyData
+      }
+    },
+}
+</script>
+
+<style scoped lang="scss">
+    @import url("~/assets/styles/body-layout.scss");
+</style>
