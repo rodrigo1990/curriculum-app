f57906150aa728f2b87f72900a3b43a630af41f5
rodrigo1990 <mcd77.1990@gmail.com>
2024-11-02 20:05:52 +0100

    ButtonsColumn in composition API

--- curriculum-frontend/components/Buttons/ButtonsColumn.vue
+++ curriculum-frontend/components/Buttons/ButtonsColumn.vue
@@ -8,22 +8,15 @@
   </div>
 </template>
 
-<script>
+<script setup>
 import ButtonColumn from './ButtonColumn.vue';
-
-export default {
-  name: 'ButtonsColumn',
-  components: {
-    ButtonColumn
-  },
-  props: {
-    msg: String,
-    buttonsArray: {
-      type: Array,
-      required: true
-    }
+defineProps({
+  msg: String,
+  buttonsArray: {
+    type: Array,
+    required: true
   }
-}
+})
 </script>
 
 <style scoped lang="scss">
