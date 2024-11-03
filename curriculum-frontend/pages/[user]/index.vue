f87a78b363bfc77255b287ddb10a85636345777b
rodrigo1990 <mcd77.1990@gmail.com>
2024-11-03 06:50:50 +0100

    reforward to main page

--- curriculum-frontend/pages/[user]/index.vue
+++ curriculum-frontend/pages/[user]/index.vue
@@ -2,3 +2,9 @@
     <NuxtLayout>
     </NuxtLayout>
 </template>
+<script setup>
+const route = useRoute()
+if(route.params.id == null)
+    navigateTo('/'+route.params.user+'/content/1')
+
+</script>
