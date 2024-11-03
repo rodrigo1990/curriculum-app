afc5f5bb2de934b6cfacd7088168ae908f78e703
rodrigo1990 <mcd77.1990@gmail.com>
2024-11-03 21:43:10 +0100

    move header to header component

---
+++ curriculum-frontend/components/Header/Main.vue
@@ -0,0 +1,27 @@
+<template>
+    <section id="header">
+        <div class="container-fluid">
+        <div class="header">
+            <div class="row">
+                <div class="col-lg-6"> 
+                    <!--  <ButtonHeader :contentId="btnsHeaderData[0].id" description="This is a logo" />-->
+                    <ButtonHeader :contentId="btnsState[0].id" description="this is a logo" />
+                </div>
+                <div class="col-lg-6">
+                    <ButtonsListHeader :btns="btnsState"/>
+                </div> 
+            </div>
+        </div>
+        </div>
+  </section>
+</template>
+
+<script setup>
+    const props = defineProps({
+        btns:{
+            type:Array,
+            required:true
+        }
+    })
+    const btnsState = ref(props.btns)
+</script>
--- curriculum-frontend/layouts/default.vue
+++ curriculum-frontend/layouts/default.vue
@@ -1,20 +1,6 @@
 <template>
 <div>
-  <section id="header">
-    <div class="container-fluid">
-      <div class="header">
-        <div class="row">
-            <div class="col-lg-6"> 
-                <!--  <ButtonHeader :contentId="btnsHeaderData[0].id" description="This is a logo" />-->
-                <ButtonHeader :contentId="btnsHeaderData[0].id" description="this is a logo" />
-            </div>
-            <div class="col-lg-6">
-              <ButtonsListHeader :btns="btnsHeaderData"/>
-            </div> 
-        </div>
-      </div>
-    </div>
-  </section>
+  <HeaderMain :btns="btnsHeaderData"/>
   <section id="body">
     <div class="container-fluid">
       <div class="row">
@@ -50,18 +36,17 @@
 
 <style lang="scss">
   @import url("~/assets/styles/app.scss");
-    body {
-      -webkit-font-smoothing: antialiased;
-      -moz-osx-font-smoothing: grayscale;
-
-      background: rgb(74, 34, 0);
-      background: linear-gradient(164deg, rgba(74, 34, 0, 1) 0%, rgba(10, 10, 12, 1) 50%);
-      background-repeat: no-repeat;
-      background-attachment: fixed;
+  @import url("~/assets/styles/body-layout.scss");
+  body {
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    background: rgb(74, 34, 0);
+    background: linear-gradient(164deg, rgba(74, 34, 0, 1) 0%, rgba(10, 10, 12, 1) 50%);
+    background-repeat: no-repeat;
+    background-attachment: fixed;
   }
 </style>
 <style scoped lang="scss">
-  @import url("~/assets/styles/body-layout.scss");
   section#body{
     margin-top:6rem;
   }
