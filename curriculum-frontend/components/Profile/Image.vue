ef6982a36f223900eb566b76cc8b9ce957d80ebe
rodrigo1990 <mcd77.1990@gmail.com>
2024-11-03 18:30:21 +0100

    loader spinner

--- curriculum-frontend/components/Profile/Image.vue
+++ curriculum-frontend/components/Profile/Image.vue
@@ -1,7 +1,12 @@
 <template>
-  <div class="profile-img-container" fallback-tag="span" fallback="Loading...">
+  <div class="profile-img-container">
+    
         <ClientOnly>
           <div class="profile-img" :style="{ 'background-image': 'url('+getImagePath(path)+'' }"> </div>
+          <template #fallback>
+            <!-- this will be rendered on server side -->
+              <span class="loader"></span>>
+          </template>
         </ClientOnly>
   </div>
 </template>
@@ -21,5 +26,6 @@
 </script>
 
 <style scoped lang="scss">
+    @import url("~/assets/styles/loader.scss");
     @import url("~/assets/styles/profile-image.scss");
 </style>
