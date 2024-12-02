30e2fd11bea19daac543803ddefc96f53860a30a
rodrigo1990 <mcd77.1990@gmail.com>
2024-12-02 05:39:42 +0100

    Render movie mongoDb sample data

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\MovieController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class, 'index']);
+Route::get('/browse_movies/', [MovieController::class, 'show']);
