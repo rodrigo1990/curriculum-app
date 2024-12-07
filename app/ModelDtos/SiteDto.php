d80aa868ec31651c1e56884bfcc353be87efb534
rodrigo1990 <mcd77.1990@gmail.com>
2024-12-07 23:19:52 +0100

    create siteDto

---
+++ app/ModelDtos/SiteDto.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\ModelDtos;
+
+use App\Models\Site;
+use App\Models\SiteStyles;
+use App\Models\User;
+use Carbon\Carbon;
+use Spatie\LaravelData\Data;
+
+class SiteDto extends Data
+{
+    public function __construct(
+        public int $id,
+        public string $name,
+        public int $userId,
+        public Carbon $createdAt,
+        public Carbon $updatedAt,
+        public SiteStyles $styles
+
+    ) {
+    }
+}
