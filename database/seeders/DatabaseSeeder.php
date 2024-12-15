9482f5318c73915f20d8c8b1e4cf1ea8f7b83712
rodrigo1990 <mcd77.1990@gmail.com>
2024-12-15 21:46:06 +0100

    seed database in all tables

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -37,11 +37,156 @@ class DatabaseSeeder extends Seeder
 
 
         DB::table('sites')->insert([
+            [
+                'id' => 1,
+                'name' => 'Testing site',
+                'user_id' => 1,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'id' => 2,
+                'name' => 'Testing site',
+                'user_id' => 1,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ]
+        ]);
+
+
+        DB::table('headers')->insert([
             'id' => 1,
-            'name' => 'Testing site',
-            'user_id' => 1,
+            'site_id' => 1,
             'created_at' => now(),
             'updated_at' => now(),
+        ]);
+
+
+        DB::table('pages')->insert([
+            [
+                'id' => 1,
+                'title' => 'Whoami',
+                'slug' => 'whoami',
+                'site_id' => 1,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'id' => 2,
+                'title' => 'Academics',
+                'slug' => 'academics',
+                'site_id' => 1,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'id' => 3,
+                'title' => 'Services',
+                'slug' => 'services',
+                'site_id' => 1,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'id' => 4,
+                'title' => 'Github',
+                'slug' => 'github',
+                'site_id' => 1,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'id' => 5,
+                'title' => 'Callme!',
+                'slug' => 'callme',
+                'site_id' => 1,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+
+        ]);
+
+
+        DB::table('contents')->insert([
+            [
+                'id' => 1,
+                'page_id' => 1,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'id' => 2,
+                'page_id' => 2,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'id' => 3,
+                'page_id' => 3,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'id' => 4,
+                'page_id' => 4,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'id' => 5,
+                'page_id' => 5,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+        ]);
+
+
+        DB::table('buttons')->insert([
+            [
+                'id' => 1,
+                'name' => 'Whoami',
+                'page_id' => 1,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'id' => 2,
+                'name' => 'Academics',
+                'page_id' => 2,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'id' => 3,
+                'name' => 'Services',
+                'page_id' => 3,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'id' => 4,
+                'name' => 'Github',
+                'page_id' => 4,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'id' => 5,
+                'name' => 'Callme!',
+                'page_id' => 5,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+        ]);
+
+
+        DB::table('buttons_header')->insert([
+            [
+                'button_id' => 5,
+                'header_id' => 1,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ]
         ]);
     }
 }
