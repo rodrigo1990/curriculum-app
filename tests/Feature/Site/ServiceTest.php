adf4d3d226bdebbed367d9b3db95388cbe1ccfd5
rodrigo1990 <mcd77.1990@gmail.com>
2024-12-16 05:17:46 +0100

    get site by user name tests

--- tests/Feature/Site/ServiceTest.php
+++ tests/Feature/Site/ServiceTest.php
@@ -4,6 +4,7 @@ namespace Tests\Feature\Site;
 
 use App\Repositories\SiteMongoRepository;
 use App\Repositories\SiteRepository;
+use App\Repositories\UserRepository;
 use App\Services\SiteService;
 use Illuminate\Support\Facades\Artisan;
 use Tests\TestCase;
@@ -15,12 +16,23 @@ class ServiceTest extends TestCase
     protected function setUp(): void{
         parent::setUp();
 //        Artisan::call('db:seed');
-        $this->service = new SiteService(new SiteRepository(), new SiteMongoRepository());
+        $this->service = new SiteService(new SiteRepository(), new SiteMongoRepository(), new UserRepository());
     }
 
     public function test_service_respose_has_mongo_records(): void
     {
         $site = $this->service->getSite(1);
+
+        if(!empty($site->styles))
+            $this->assertTrue(true);
+        else
+            $this->assertFalse(true);
+    }
+
+
+    public function test_get_site_by_username_has_mongo_records(): void
+    {
+        $site = $this->service->getSiteByUser('rodrigo1990');
 
         if(!empty($site->styles))
             $this->assertTrue(true);
