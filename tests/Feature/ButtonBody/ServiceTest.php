36d099b8d018c285ab24ca651f4f5b70853142b2
rodrigo1990 <mcd77.1990@gmail.com>
2024-12-19 04:28:07 +0100

    test buttons body

---
+++ tests/Feature/ButtonBody/ServiceTest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace ButtonBody;
+
+use App\Models\Body;
+use App\Repositories\BodyMongoRepository;
+use App\Repositories\ButtonsBodyMongoRepository;
+use App\Repositories\ButtonsRepository;
+use App\Repositories\SiteRepository;
+use App\Repositories\UserRepository;
+use App\Services\ButtonsBodyService;
+use App\Services\SiteService;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class ServiceTest extends TestCase
+{
+    private $service;
+
+    use RefreshDatabase;
+
+    protected function setUp(): void{
+        parent::setUp();
+//        Artisan::call('db:seed');
+        $this->service = new ButtonsBodyService(
+            new ButtonsRepository(),
+            new ButtonsBodyMongoRepository(),
+        );
+    }
+
+    public function test_get_button_body_by_body_has_styles(): void
+    {
+        $buttons = $this->service->getButtonsBodyByBody(Body::find(1));
+
+        foreach ($buttons as $button) {
+            if(empty($button->styles))
+                $this->assertFalse(true);
+        }
+
+        $this->assertTrue(true);
+    }
+
+}
