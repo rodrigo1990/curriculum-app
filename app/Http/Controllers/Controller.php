ed5a4e61798953b219ab0ba8bfa5cfc7aae583b4
rodrigo1990 <mcd77.1990@gmail.com>
2024-12-25 07:41:29 +0100

    impelement better try catch fix

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,7 +6,7 @@ abstract class Controller
 {
     public function getNotFoundConditional(\Throwable $exception):bool
     {
-        if(get_class($exception) == 'TypeError' && $e->getCode() == 0)
+        if(get_class($exception) == 'TypeError' && $exception->getCode() == 0)
             return true;
         else
             return false;
