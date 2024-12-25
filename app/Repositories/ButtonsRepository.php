f638485fd9d70415a001c21118aa069048b0ad3e
rodrigo1990 <mcd77.1990@gmail.com>
2024-12-25 07:47:54 +0100

    Fix

--- app/Repositories/ButtonsRepository.php
+++ app/Repositories/ButtonsRepository.php
@@ -5,15 +5,16 @@ namespace App\Repositories;
 use App\Models\Button;
 use App\Models\ButtonBody;
 use App\Models\ButtonHeader;
+use Illuminate\Database\Eloquent\Collection;
 
 class ButtonsRepository
 {
-    public function getBodyButtons(int $bodyId):ButtonBody
+    public function getBodyButtons(int $bodyId):Collection
     {
         return ButtonBody::where('body_id', $bodyId)->with('button')->get();
     }
 
-    public function getHeaderButtons(int $headerId):ButtonHeader
+    public function getHeaderButtons(int $headerId):Collection
     {
         return ButtonHeader::where('header_id', $headerId)->with('button')->get();
     }
