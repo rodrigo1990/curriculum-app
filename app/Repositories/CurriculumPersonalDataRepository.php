097adb0dfecdf8dea7d987cd7581db58af7b5126
rodrigo1990 <mcd77.1990@gmail.com>
2025-02-22 04:59:56 +0100

    save or update

--- app/Repositories/CurriculumPersonalDataRepository.php
+++ app/Repositories/CurriculumPersonalDataRepository.php
@@ -8,7 +8,11 @@ class CurriculumPersonalDataRepository
 {
     public function create(string $name, string $surname, string $email, string $telephone, ?string $birthday): void
     {
-        $curriculumUserData = new CurriculumUserData();
+        if(CurriculumUserData::where('site_id',1)->exists())
+            $curriculumUserData = CurriculumUserData::where('site_id',1)->first();
+        else
+            $curriculumUserData = new CurriculumUserData();
+
         $curriculumUserData->name = $name;
         $curriculumUserData->surname = $surname;
         $curriculumUserData->email = $email;
